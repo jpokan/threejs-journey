810cc4c8a07fbc16d7aa35d2539cc1c65c80b935
Jorge Ou <8276144+Topomon@users.noreply.github.com>
2021-03-31 11:31:29 -0500

    finished lesson 3

--- 03-basic-scene/scripts.js
+++ 03-basic-scene/scripts.js
@@ -1,1 +1,36 @@
-console.log(THREE)
+// Scene
+const scene = new THREE.Scene()
+
+// Red cube
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+// Sizes
+const sizes = {
+	width: 800,
+	height: 600,
+}
+// Perspective Camera
+/* 
+	First parameter is field of view - Vertical vision angle in degrees
+	Second parameter is aspect ratio - Size of a container
+*/
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+/*
+	At this point, the camera is positioned in the center and inside the cube, hence it will produce a black scene.
+*/
+// Positioning the camera
+camera.position.z = 3
+scene.add(camera)
+
+// Renderer
+const canvas = document.querySelector('.webgl')
+
+const renderer = new THREE.WebGLRenderer({
+	canvas: canvas,
+})
+
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
